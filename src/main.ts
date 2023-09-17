6b4ab97e9a940f9389bfdf6d59350a09a671626e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-09-17 17:30:20 +0100

    Fix files

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import * as core from '@actions/core'
 import fs from 'fs'
 import util from 'util'
 
-async function run(): Promise<void> {
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
   try {
-    const filePath = core.getInput('path')
-    const encoding = core.getInput('encoding')
+    const filePath: string = core.getInput('path')
+    const encoding: string | null = core.getInput('encoding')
     const readFile = util.promisify(fs.readFile)
-    const contents = await readFile(filePath, encoding)
+    const contents = await readFile(filePath, { encoding: encoding })
     core.info(`File contents:\n${contents}`)
     core.setOutput('contents', contents)
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
-run()
